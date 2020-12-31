3c521ed558b8a1c6ec751e24eaa15a6da24c3b2a
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2020-12-31 12:55:58 +0530

    added day 14 2020 part 1

--- 2020/Day 14/main.py
+++ 2020/Day 14/main.py
@@ -1,15 +1,14 @@
-with open('data.txt', 'r') as f:
-	data = f.read().strip().split('\n')
-
-mem = {}
-
-# for num in data:
-# 	if num.startswith('mask'):
-# 		_, mask = num.split('=')
-# 	elif num.startswith('mem'):
-# 		
-
 lines = open('data.txt', 'r').read().splitlines()
-instructions = [line.replace('mem', '').replace('[', '').replace(']', '').split(' = ') for line in lines]
+instructions = [line.replace('mem', '').replace('[','').replace(']', '').split(' = ') for line in lines]
 
-print(instructions)
+def part1(instructions):
+	set0mask, set1mask, mem = 0, 0, {}
+	for instruction, value in instructions:
+		if instruction == 'mask':
+			set0mask, set1mask = int(value.replace('X', '1'),2), int(value.replace('X', '0'),2)
+		else:
+			address, value = int(instruction), int(value)
+			mem[address] = value & set0mask | set1mask
+	return sum(mem.values())
+
+print(f"1. {part1(instructions)}")
