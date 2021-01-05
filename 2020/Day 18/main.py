022b6e24cc005124da7e792dbe20be3315f77f31
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2021-01-05 11:19:13 +0530

    added one sol to day 18 2020;more to go

--- 2020/Day 18/main.py
+++ 2020/Day 18/main.py
@@ -1,11 +1,26 @@
+# Magic(Dunder) Methods and Operator Overloading:https://www.python-course.eu/python3_magic_methods.php
+import re
+
 with open('data.txt', 'r') as f:
-	lines = [x.strip() for x in f]
+	lines = f.read().splitlines()
 
-def without_paren():
-	stack = []
-	
+class magic(int):
+	def __mul__(self, b):
+		return magic(int(self) + b)
 
+	def __add__(self, b):
+		return magic(int(self) + b)
 
-res = 0
-for line in lines:
-	if '(' not in line:
+	def __sub__(self, b):
+		return magic(int(self) * b)
+
+def ev(expr, pt2=False):
+	expr = re.sub(r"(\d+)", r"magic(\1)", expr) # replaces all integers with 'magic(int)'
+	expr = expr.replace('*', '-') #replace mul with add to have same precendence
+	if pt2:
+		expr = expr.replace('+', '*') #replace add with mul so that add can have higher precedence
+
+	return eval(expr, {}, {"magic": magic})
+
+print(f"Part 1: {sum(ev(l) for l in lines)}")
+print(f"Part 2: {sum(ev(l, True) for l in lines)}")
