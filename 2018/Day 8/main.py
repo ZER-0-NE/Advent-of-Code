efd4daa5e523f78e2897e81d82b0afefb0051a68
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2021-01-13 16:02:36 +0530

    added day 8 multiple sols 2018

--- 2018/Day 8/main.py
+++ 2018/Day 8/main.py
@@ -26,3 +26,50 @@ total, value, remaining = parse(data)
 
 print(f"1: {total}")
 print(f"2: {value}")
+
+# another sol; credits to michaelmarsalek
+
+
+def sumtree(t):
+	ch = t.pop(0)
+	md = t.pop(0)
+	return sum(sumtree(t) for _ in range(ch)) + sum(t.pop(0) for _ in range(md))
+
+print(f"1. {sumtree(data)}")
+
+def val(t):
+	ch = t.pop(0)
+	md = t.pop(0)
+	vals = [val(t) for _ in range(ch)]
+	mdata = [t.pop(0) for _ in range(md)]
+
+	if ch == 0:
+		return sum(mdata)
+	return sum(vals[i-1] for i in mdata if i-1 in range(ch))
+
+print(f"2. {val(data)}")
+
+
+# another sol; credits to mastermedo
+
+def solve(child, meta):
+	if child == 0:
+		return sum(next(dta) for _ in range(meta))
+
+	value = 0
+	children = {}
+
+	for i in range(1, child+1):
+		value += solve(next(dta), next(dta)) # comment for pt2
+		# children[i] = solve(next(dta), next(dta))
+
+	for _ in range(meta):
+		value += next(dta)# comment for pt2
+		# value += children.get(next(dta), 0)
+
+	return value
+
+
+dta = iter(map(int, open('data.txt').read().split()))
+
+print(f"1. {solve(next(dta), next(dta))}")
