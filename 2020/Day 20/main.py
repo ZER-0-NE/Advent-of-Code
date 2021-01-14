ec5a41f62447c6031afc0e93711b3f4e2622da83
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2021-01-14 16:12:12 +0530

    added day 21 2020

---
+++ 2020/Day 20/main.py
@@ -0,0 +1,6 @@
+# can't right now
+# some links: 
+# -https://github.com/bylexus/adventofcode2020/blob/master/20-jurassic-jigsaw.py
+# -https://github.com/Dpalme/Advent-Of-Code/blob/master/2020/day20.py
+# -https://gist.github.com/sciyoshi/4a297cd965baad369f5f544029a10dbc
+# -https://github.com/Rtchaik/AoC-2020/blob/master/Day20/solution.py
--- 2020/Day 21/main.py
+++ 2020/Day 21/main.py
@@ -1,2 +1,36 @@
 with open('data.txt', 'r') as f:
-	rows = f.read().strip().split('\n')
+	data = f.read().splitlines()
+
+allergens = {}
+ingredients = []
+
+for e in data:
+	a,b = e.rstrip(')').split('(contains ')
+	# print(a, b)
+	ingr = a.split()
+	ingredients.append(ingr)
+	for a in b.split(", "):
+		if a not in allergens:
+			allergens[a] = set(ingr)
+		else:
+			allergens[a] &= set(ingr)
+# print(allergens)
+all_allergs = set(e for a in allergens.values() for e in a)
+print(f"1. {sum(i not in all_allergs for ingr in ingredients for i in ingr)}")
+
+def remove(a, allergs):
+	s = set()
+	for k in allergs:
+		
+		if len(allergs[k])>1 and a in allergs[k]:
+			allergs[k].remove(a)
+			if len(allergs[k]) == 1:
+				s |= allergs[k]
+	return s
+
+r = next(list(v) for v in allergens.values() if len(v) == 1)
+
+while r:
+	r += list(remove(r.pop(), allergens))
+
+print(f"2. {','.join(str(i.pop()) for a,i in sorted((k,v) for k, v in allergens.items()))}")
