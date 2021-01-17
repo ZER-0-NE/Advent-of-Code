70b68dc9408a939728107ac547b85f7369651039
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2021-01-17 11:39:27 +0530

    added day 22 2020

--- 2020/Day 21/main.py
+++ 2020/Day 21/main.py
@@ -34,3 +34,41 @@ while r:
 	r += list(remove(r.pop(), allergens))
 
 print(f"2. {','.join(str(i.pop()) for a,i in sorted((k,v) for k, v in allergens.items()))}")
+
+# another sol
+
+import re
+
+pat = re.compile(r"^([a-z\s]+) \(contains ([a-z,\s]+)\)$")
+
+ingredients = []
+allergen_reference = dict()
+
+for i, line in enumerate(open('data.txt')):
+	ing, allerg = pat.match(line).groups()
+	ingredients.append(ing.split())
+	for al in allerg.split(", "):
+		this_list = allergen_reference.get(al, list())
+		this_list.append(i)
+		allergen_reference[al] = this_list
+
+allergen_candidates = {
+	key: set.intersection(*[set(ingredients[k]) for k in allergen_reference[key]])
+	for key, value in allergen_reference.items()
+}
+
+no_ingredient = set(i for l in ingredients for i in l).difference(set.union(*(allergen_candidates.values())))
+
+print(f"1. {len([i for l in ingredients for i in l if i in no_ingredient])}")
+
+visited = set()
+while any(len(s) > 1 for s in allergen_candidates.values()):
+    for key, value in allergen_candidates.items():
+        if len(value) == 1 and key not in visited:
+            visited.add(key)
+            break
+    for key2, value2 in allergen_candidates.items():
+        if key2 != key:
+            value2.difference_update(value)
+    
+print(",".join(next(iter(v)) for _, v in sorted(allergen_candidates.items(), key=lambda v: v[0])))
--- 2020/Day 22/main.py
+++ 2020/Day 22/main.py
@@ -1,3 +1,4 @@
+# part 1
 from collections import deque
 
 with open('data.txt', 'r') as f:
@@ -21,3 +22,76 @@ while q2:
 	res += len(q2) * q2.popleft()
 
 print(f"1. {res}")
+
+# another sol for part 1 
+
+# Deck = deque[int]
+
+with open('data.txt') as file:
+	player_input = file.read().strip().split('\n\n')
+
+decks = [deque(map(int, player.split('\n')[1:])) for player in player_input]
+
+round_number = 0
+winner, loser = 0, 0
+
+# print(decks)
+
+while all(decks):
+	round_number += 1
+	drawn_cards = [deck.popleft() for deck in decks]
+	# print(f"Round {round_number}: cards: {drawn_cards}, decks: {decks}")
+	winner, loser = (0, 1) if drawn_cards[0]>drawn_cards[1] else (1, 0)
+	for i in winner, loser:
+		decks[winner].append(drawn_cards[i])
+print(f"1. Player {winner+1} wins")
+print(sum([(i + 1) * card for i, card in enumerate(reversed(decks[winner]))]))
+
+
+# another sol for part 1 and 2
+
+player1_line, player2_line = open('data.txt').read().replace('\r\n', '\n').replace('\n\n', '|').replace('\n', ' ').split('|')
+
+player1 = list(map(int, player1_line.split(':')[1].split()))
+player2 = list(map(int, player2_line.split(':')[1].split()))
+
+def multiply(player):
+	score, multiplier = 0, 1
+	while player:
+		score += multiplier * player.pop()
+		multiplier += 1
+	return score
+
+def part1(player1, player2):
+	player1, player2 = player1[:], player2[:]
+	while player1 and player2:
+		player1_top, player2_top = player1.pop(0), player2.pop(0)
+		player1 += [player1_top, player2_top] if player1_top>player2_top else []
+		player2 += [player2_top, player1_top] if player2_top>player1_top else []
+	return player1 if player1 else player2
+
+winner1 = part1(player1, player2)
+print(f"1. {multiply(winner1)}")
+
+def part2(player1, player2):
+	states = set()
+	while player1 and player2:
+	    state = (tuple(player1), tuple(player2))
+	    if state in states:
+	        return (1, player1)
+
+	    player1_top, player2_top = player1.pop(0), player2.pop(0)
+
+	    if player1_top <= len(player1) and player2_top <= len(player2):
+	        winner, _ = part2(player1[:player1_top], player2[:player2_top])
+	    else:                
+	        winner = 1 if player1_top > player2_top else 2
+
+	    player1 += [player1_top, player2_top] if winner == 1 else []
+	    player2 += [player2_top, player1_top] if winner == 2 else []
+	    states.add(state)
+
+	return (1, player1) if player1 else (2, player2)
+
+_, winner2 = part2(player1, player2)
+print(f"2. {multiply(winner2)}")
